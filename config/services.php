e0163045bcf4bad873387c23a3711f17ee567322
saranyoo00200 <68580575+saranyoo00200@users.noreply.github.com>
2021-10-17 06:21:46 +0700

    update

--- config/services.php
+++ config/services.php
@@ -32,12 +32,12 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://localhost/laravel-running/public/login/google/callback',
+        'redirect' => 'http://localhost/Laravel-Running/public/login/google/callback',
     ],
 
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => 'http://localhost/laravel-running/public/login/facebook/callback',
+        'redirect' => 'http://localhost/Laravel-Running/public/login/facebook/callback',
     ],
 ];
